05c4869c057ea9c0fbea4e79bb400ad837f04192
Anurag Sachan <anuragsachan07@gmail.com>
2021-01-05 12:23:08 +0530

    Adding a new message.

--- src/main/java/com/anurag/springit/SpringitApplication.java
+++ src/main/java/com/anurag/springit/SpringitApplication.java
@@ -9,6 +9,7 @@ public class SpringitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 		System.out.println("Welcome to springit");
+		System.out.println("This is another comment");
 	}
 
 }
