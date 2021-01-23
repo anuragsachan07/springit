c22a90fc02afeef262518a310f7235eb9fdf6ece
Git Bucket <gitbucket@yandex.com>
2021-01-23 11:38:01 +0530

    added more code

--- src/main/java/com/anurag/springit/SpringitApplication.java
+++ src/main/java/com/anurag/springit/SpringitApplication.java
@@ -1,14 +1,18 @@
 package com.anurag.springit;
 
+import com.anurag.springit.config.SpringitProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-		System.out.println("Welcome to springit");
+		System.out.println("Welcome to springit project");
+		System.out.println("something new");
 	}
 
 }
